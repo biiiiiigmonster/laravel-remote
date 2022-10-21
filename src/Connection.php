0a1c85d56ed6eeb3038268878b0130cdf44a98ab
陆云峰 <v-luyunfeng@xiaomi.com>
2022-10-21 15:19:24 +0800

    init

--- src/Connection.php
+++ src/Connection.php
@@ -5,6 +5,8 @@ namespace BiiiiiigMonster\Remote;
 
 use Illuminate\Database\Connection as BaseConnection;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Grammars\MySqlGrammar;
+use PHPSQLParser\PHPSQLParser;
 
 class Connection extends BaseConnection
 {
@@ -13,13 +15,15 @@ class Connection extends BaseConnection
      */
     public function query(): Builder
     {
-        return new Builder($this, new Grammar());
+        return new Builder($this, new MySqlGrammar());
     }
 
     public function insert($query, $bindings = [])
     {
+        $parser = new PHPSQLParser($query);
         var_dump($query);
         var_dump($bindings);
+
         die;
     }
 }
